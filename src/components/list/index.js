2b184f03c3103181c26aac32267c986a90bd68b8
Scott Bowler <scott.bowler@learningfuze.com>
2018-06-26 15:42:03 -0700

    Toggle complete in-progress

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Item from './item';
+import './list.css';
 
 export default props => {
 
     const listElements = props.data.map((item, index) => {
-        return <Item key={item._id} title={item.title} delete={props.delete.bind(this, item._id)} />
+        return <Item key={item._id} complete={item.complete} title={item.title} delete={props.delete.bind(this, item._id)} />
     })
 
     return (
