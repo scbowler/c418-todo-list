fc71b1efa4db9c0bb84af17c269e5199ee820cb7
Scott Bowler <scott.bowler@learningfuze.com>
2018-06-26 15:01:18 -0700

    Delete functionality working

--- src/components/list.js
+++ src/components/list.js
@@ -4,7 +4,7 @@ import Item from './item';
 export default props => {
 
     const listElements = props.data.map((item, index) => {
-        return <Item key={item._id} title={item.title} />
+        return <Item key={item._id} title={item.title} delete={props.delete.bind(this, item._id)} />
     })
 
     return (
